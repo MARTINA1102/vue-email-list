f01f32b41344338dc58fc64e315c35efa06fa78c
MARTINA1102 <martinamiccoli11@gmail.com>
2022-11-22 13:54:54 +0100

    esercizio completo

---
+++ script.js
@@ -0,0 +1,33 @@
+/*Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.*/
+
+new Vue({
+	el:'#root',
+	data:{
+		email:[],
+	},
+	methods:{
+		getEmail(){
+			for (let i=0; i<10;i++){
+				axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+				.then(axiosResponse=>{
+					this.email.push(axiosResponse.data.response);
+				})
+			}
+		}
+	},	
+		
+		
+	
+
+	});
+	
+	
+
+
+
+
+
