d4ca9d422728e5813223db0e3277aa257216a2ea
MARTINA1102 <martinamiccoli11@gmail.com>
2022-11-23 10:12:43 +0100

    completo con bonus

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ new Vue({
 	},
 	methods:{
 		getEmail(){
+			this.email=[];
 			for (let i=0; i<10;i++){
 				axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
 				.then(axiosResponse=>{
